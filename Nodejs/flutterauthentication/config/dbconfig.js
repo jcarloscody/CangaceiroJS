8869f6bbd07e7ad0de663049bf86628aae0e06b0
Josue <josuecarlosos2@gmail.com>
2022-01-21 17:25:08 -0300

    ...

--- Nodejs/flutterauthentication/config/dbconfig.js
+++ Nodejs/flutterauthentication/config/dbconfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-    secret: 'yoursecret',
-    database: 'mongodb+srv://josue:teste@cluster0.p0po7.mongodb.net/JosuCluster?retryWrites=true&w=majority'
+    secret: 'word secred',
+    database: 'path'
 }
