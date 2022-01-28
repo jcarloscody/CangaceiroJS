c9d62353fbad15bd694cf87de476020417c694ac
Josue <josuecarlosos2@gmail.com>
2022-01-28 00:40:45 -0300

    working with express - static files middleware

--- Nodejs/WorkingWithExpress/app.js
+++ Nodejs/WorkingWithExpress/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path')
 
 const app = express();
 
@@ -7,12 +8,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))//servindo arquivos estaticos
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('Page not found')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404erro.html'))
 })
 
 app.listen(3000);
