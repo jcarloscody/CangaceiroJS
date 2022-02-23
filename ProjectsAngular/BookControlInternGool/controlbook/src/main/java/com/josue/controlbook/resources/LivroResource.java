99f0333ca2456dfb577b451623a925f09de67ae8
Josue <josuecarlosos2@gmail.com>
2022-02-23 13:27:10 -0300

    POST - livros

--- ProjectsAngular/BookControlInternGool/controlbook/src/main/java/com/josue/controlbook/resources/LivroResource.java
+++ ProjectsAngular/BookControlInternGool/controlbook/src/main/java/com/josue/controlbook/resources/LivroResource.java
@@ -1,5 +1,6 @@
 package com.josue.controlbook.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,11 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.josue.controlbook.domain.Livro;
 import com.josue.controlbook.dtos.LivroDTO;
@@ -50,6 +53,13 @@ public class LivroResource {
 	public ResponseEntity<Livro> upDatePatch(@PathVariable Integer id, @RequestBody Livro obj){
 		Livro newObj = livroService.upDate(id, obj);
 		return ResponseEntity.ok().body(newObj);
+	}
+	
+	@PostMapping  //8080/livros?categoria=3
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer cat_id, @RequestBody Livro obj){
+		Livro newObj = livroService.create(cat_id, obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	/*
--- ProjectsAngular/BookControlInternGool/controlbook/src/main/java/com/josue/controlbook/service/LivroService.java
+++ ProjectsAngular/BookControlInternGool/controlbook/src/main/java/com/josue/controlbook/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.josue.controlbook.domain.Categoria;
 import com.josue.controlbook.domain.Livro;
 import com.josue.controlbook.repositories.LivroRepository;
 import com.josue.controlbook.service.exceptions.ObjectNotFoundException;
@@ -15,19 +16,19 @@ public class LivroService {
 
 	@Autowired
 	private LivroRepository livroRepository;
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
-	
-	
+
 	public Livro findById(Integer id) {
-		Optional<Livro> obj = this.livroRepository.findById(id); //optional pq pode encontrar ou nao
-		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+		Optional<Livro> obj = this.livroRepository.findById(id); // optional pq pode encontrar ou nao
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
 	}
-	
-	//retorna todos os livros pela categoria
-	public List<Livro> findAll(Integer id_cat){
-		categoriaService.findById(id_cat); //caso não exita retorna uma exceção
+
+	// retorna todos os livros pela categoria
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat); // caso não exita retorna uma exceção
 		return livroRepository.findAllByIdCategoria(id_cat);
 	}
 
@@ -42,33 +43,11 @@ public class LivroService {
 		newObj.setTexto(obj.getTexto());
 		newObj.setNome_autor(obj.getNome_autor());
 	}
-	
-	/*
-	public List<Livro> findAll(){
-		return livroRepository.findAll();
-	}
-	
-	public Livro create(Livro obj) {
+
+	public Livro create(Integer cat_id, Livro obj) {
 		obj.setId(null);
+		Categoria cat = categoriaService.findById(cat_id);
+		obj.setCategoria(cat);
 		return livroRepository.save(obj);
 	}
-	
-	public Livro upDate(Integer id, LivroDTO objDTO) {
-		Livro obj = findById(id);
-		obj.setdescricao(objDTO.getDescricaoString());
-		obj.setNome(objDTO.getNome());
-		return livroRepository.save(obj);
-	}
-
-
-	public void delete(Integer id) {
-		findById(id);
-		try {
-			livroRepository.deleteById(id);
-			
-		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityViolationExceptionn("Livro não pode ser deletado pq tem livros associados");
-		}
-		
-	}*/
 }
