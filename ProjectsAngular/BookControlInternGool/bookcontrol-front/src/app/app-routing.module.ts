692927d8125df9d6a532681b829553d6699ab5ac
Josue <josuecarlosos2@gmail.com>
2022-02-25 18:09:13 -0300

    criando o compoennte de categoria - delete

--- ProjectsAngular/BookControlInternGool/bookcontrol-front/src/app/app-routing.module.ts
+++ ProjectsAngular/BookControlInternGool/bookcontrol-front/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { NgModule } from '@angular/core';
@@ -7,7 +8,8 @@ import { HomeComponent } from './components/views/home/home.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'categorias', component: CategoriaReadComponent},
-  {path: 'categorias/create', component: CategoriaCreateComponent}
+  {path: 'categorias/create', component: CategoriaCreateComponent},
+  {path: 'categorias/delete/:id', component: CategoriaDeleteComponent}
 ];
 
 @NgModule({
