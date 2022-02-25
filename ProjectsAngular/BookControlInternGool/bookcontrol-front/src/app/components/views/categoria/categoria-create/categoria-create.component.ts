03b38e3dd118b7ca8a7f92d8516faca09dcb6f0b
Josue <josuecarlosos2@gmail.com>
2022-02-25 17:48:15 -0300

    colocando o snackBar no service de categoria, navegacao de volta, terminando o categoria create

--- ProjectsAngular/BookControlInternGool/bookcontrol-front/src/app/components/views/categoria/categoria-create/categoria-create.component.ts
+++ ProjectsAngular/BookControlInternGool/bookcontrol-front/src/app/components/views/categoria/categoria-create/categoria-create.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { CategoriaService } from './../categoria.service';
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from '../categoria.model';
 
 @Component({
   selector: 'app-categoria-create',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaCreateComponent implements OnInit {
 
-  constructor() { }
+  categoria: Categoria = {
+    nome: "",
+    descricao: ""
+  }
+
+  constructor(private serviceCategoria: CategoriaService, private router: Router) { }
 
   ngOnInit(): void {
+ 
+  }
+
+  create(): void {
+    this.serviceCategoria.create(this.categoria).subscribe((resposta)=>{
+      this.serviceCategoria.mensagem("Categoria Inserida com Sucesso!");
+      this.router.navigate(["/categorias"]);
+    }, erro => {
+      //let erros: string = '';
+      console.warn("ERRO")
+      for (let i=0; i<erro.error.errors.length; i++) {
+        this.serviceCategoria.mensagem(`\n Coluna: ${erro.error.errors[i].fildName}  Mensagem: ${erro.error.errors[i].message} \n`)
+        //erros += `\n Coluna: ${erro.error.errors[i].fildName}  Mensagem: ${erro.error.errors[i].message} \n`;
+      }
+      //this.serviceCategoria.mensagem(erros);
+    })
   }
 
 }
--- ProjectsAngular/BookControlInternGool/bookcontrol-front/src/app/components/views/categoria/categoria.service.ts
+++ ProjectsAngular/BookControlInternGool/bookcontrol-front/src/app/components/views/categoria/categoria.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Categoria } from './categoria.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +13,19 @@ export class CategoriaService {
 
   baseUrl: String = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) { }
 
   findAll ():Observable<Categoria[]> {
     const url = `${this.baseUrl}/categorias`
     return this.http.get<Categoria[]>(url);
+  }
+
+  create(categoria: Categoria): Observable<Categoria>{
+    const url = `${this.baseUrl}/categorias`;
+    return this.http.post<Categoria>(url, categoria);
+  }
+
+  mensagem(msg: String): void {
+    this._snack.open(`${msg}`, 'OK!', {horizontalPosition: 'center', verticalPosition: 'bottom', duration: 50000,})
   }
 }
