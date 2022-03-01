28251ea195155d077718a7ba3f03ad74763713c9
Josue <josuecarlosos2@gmail.com>
2022-03-01 13:53:33 -0300

    sobre rxjs

---
+++ C/RxJSYouTube/aula1.ts
@@ -0,0 +1,30 @@
+/*
+Programação Reativa: é um paradigma baseado em fluxo de dados assincronos, e na propagacao de mudanças
+deste fluxo.
+
+PRINCÍPIOS
+1 - Elástico: Reage à demanda/carga: aplicações podem fazer uso de múltiplos cores e múltiplos servidores.
+2 - Resiliente: Reage à falhas, aplicações reagem e se recuperam de falhas de software, hardware, e de conectividade.
+3 - Message Driven: Reage à eventos (event driven): em vez de compor aplicações por múltiplas threads síncrona, sistemas são compostos de gerenciadores de eventos assíncronos e não bloqueantes.
+4 - Responsivo: Reage à usuários: aplicações que oferecem interações ricas e “tempo real” com usuários
+
+
+
+ReactiveX: basea-se nas ideias de: observer pattern, iterator pattern, programação funcional
+Observer pattern: tem os interessados, quando há alteração, os interessados sao notificados
+Iterator pattern: passa a ideia q vc pode iterar infinitamente sobre algo, tem lá uma classe e faz com que o individuo pergunte se tem algo q posso iterar, se tem um next
+Programação Funcional: 
+
+
+
+
+RxJS:::
+é uma implementacao do ReactiveX
+java: RxJava
+JS: RxJS
+PHP: RxPHP
+
+
+*/
+
+
