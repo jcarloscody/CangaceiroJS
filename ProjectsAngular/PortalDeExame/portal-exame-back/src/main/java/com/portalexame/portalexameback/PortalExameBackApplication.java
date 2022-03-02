1279375384fb637eda03ca3c1d45b8e4651aaf08
Josue <josuecarlosos2@gmail.com>
2022-03-02 07:50:40 -0300

    dando inicio ao projeto de portal de exames

---
+++ ProjectsAngular/PortalDeExame/portal-exame-back/src/main/java/com/portalexame/portalexameback/PortalExameBackApplication.java
@@ -0,0 +1,15 @@
+package com.portalexame.portalexameback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalExameBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalExameBackApplication.class, args);
+
+
+	}
+
+}
---
+++ ProjectsAngular/PortalDeExame/portal-exame-back/src/test/java/com/portalexame/portalexameback/PortalExameBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portalexame.portalexameback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalExameBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
