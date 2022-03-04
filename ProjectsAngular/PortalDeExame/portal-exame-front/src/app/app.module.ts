552e6e269106ca6da5a8359e1d5596aeb64a4cbd
Josue <josuecarlosos2@gmail.com>
2022-03-04 14:09:15 -0300

    componente de registro de usuario usando o matsnackBar e o sweetalert2

--- ProjectsAngular/PortalDeExame/portal-exame-front/src/app/app.module.ts
+++ ProjectsAngular/PortalDeExame/portal-exame-front/src/app/app.module.ts
@@ -14,6 +14,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
 
 @NgModule({
   declarations: [
@@ -32,7 +34,8 @@ import { HttpClientModule } from '@angular/common/http';
     MatFormFieldModule,
     MatIconModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ProjectsAngular/PortalDeExame/portal-exame-front/src/app/pages/signup/signup.component.ts
+++ ProjectsAngular/PortalDeExame/portal-exame-front/src/app/pages/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signup',
@@ -17,17 +19,43 @@ export class SignupComponent implements OnInit {
     phone: 0
   }
 
-  constructor(private service: UserService) { }
+  constructor(
+      private service: UserService,
+      private snackBar: MatSnackBar
+  ) {
+
+  }
 
   ngOnInit(): void {
   }
 
   formSubmit(){
+
+    if (this.user.username == '' || this.user.username==null) {
+      this.snackBar.open("o username não estar vazio!", "Ok!", {duration: 4000, horizontalPosition: 'end', verticalPosition: 'top'})
+    }
+
     this.service.addUser(this.user)
       .subscribe(
-        (v)=>{console.log(v)}, 
-        (v)=>{console.log(v)}
+        (v)=>{
+          Swal.fire("Sucess", `${v.username}, você foi incluido no sistema!`, "success");
+          this.clean();
+        }, 
+        (v)=>{
+          this.snackBar.open("Deu erro!", "Erro!", {duration: 4000, horizontalPosition: 'end', verticalPosition: 'top'})
+        }
       );
+  }
+
+
+
+  clean(){
+    this.user.email = '';
+    this.user.firstName = '';
+    this.user.lastName = '';
+    this.user.password = '';
+    this.user.phone = 0;
+    this.user.username = '';
   }
 
 }
