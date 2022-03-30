5f96c20a033725abe012cd6eaa6d3d74e0c7a845
Josue <josuecarlosos2@gmail.com>
2022-03-30 02:02:45 -0300

    reactive

---
+++ C/JSFuncionalReativo/4-princProgramFuncional/aula15-flatmap.js
@@ -0,0 +1,14 @@
+const letrasAninhadas = [
+    ['O', ['l'], 'á'],
+    [' '],
+    ['m', ['u', ['n']], 'd', 'o'],
+    ['!', '!', '!', '!']
+]
+
+const letras = letrasAninhadas.flat(Infinity)
+console.log(letras)
+const resultado = letras
+    .flatMap(l => [l, ','])
+    .reduce((a, b) => a + b)
+
+console.log(resultado)
